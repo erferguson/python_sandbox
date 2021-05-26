e7d82e94a6e9a05cba072d34ba117bc3946eea03
Eric Ferguson <ferguson.eric.r@gmail.com>
2021-05-25 19:30:14 -0700

    variables and strings, string.methods()

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,24 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Brad'
+age = 37
 
-# String Formatting
+# Concatenate
+
+# F-Strings
+print(f'Hello, my name is {name} and I am {age} years old.')
 
 # String Methods
+s = 'hello world'
+
+# Capitalize string
+print(len(s))
+
+print(s.replace('world', 'everyone'))
+
+# Count
+sub = 'h'
+# print(s.count(sub))
+
+# SPILT - into a List
+print(s.split())
--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,20 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+x = 4 # int
+y = 2.5 # float
+club = 'Everton' # str
+is_cool = True # bool
+
+# Multiple Assignment 
+x, y, club, is_cool = (1, 2.5, 'Everton', True)
+
+# Basic Math
+a = x + y
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+print(type(z), z)
